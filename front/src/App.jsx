21d1659d96b8ed87c405adf1108758d1c74b6d02
baekdonghyun <https://github.com/qorehdgus>
2026-04-17 17:53:55 +0900

    commit

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route path="/wedding/" element={<InvitationPage />} />
+        <Route path="/" element={<InvitationPage />} />
       </Route>
       <Route path="/popup" element={<PopupPage />} />
       <Route path="*" element={<NotFoundPage />} />
